a9463d270b68c8b0ee8477057bae07947ab0fd2f
kamila.mielczarek@etchuk.com <kamila.mielczarek@etchuk.com>
2019-03-31 11:18:32 +0100

    Trying with the build folder

---
+++ build/precache-manifest.692c53b9f651d02ce0bc525e1436553e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3b35c5b610356a8f7055",
+    "url": "/static/js/main.291e5807.chunk.js"
+  },
+  {
+    "revision": "028b169d5a5921ad44a5",
+    "url": "/static/js/2.a0341cfb.chunk.js"
+  },
+  {
+    "revision": "3b35c5b610356a8f7055",
+    "url": "/static/css/main.ba424629.chunk.css"
+  },
+  {
+    "revision": "de5cbbc0df2110a031cbb2c8085adfff",
+    "url": "/index.html"
+  }
+];
