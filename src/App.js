a913260d26d74f2d1111635b4aa9e795dece7c56
kamila.mielczarek@etchuk.com <kamila.mielczarek@etchuk.com>
2019-03-31 11:01:58 +0100

    initial setup of the map & location

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// React
+import React, { Component } from 'react';
+
+import Map from './Map/Map';
+
+// Styles
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return <Map className="App" />;
+  }
+}
+
+export default App;
---
+++ src/Map/Map.js
@@ -0,0 +1,44 @@
+// React core
+import React, { Component } from 'react';
+
+// Vendor
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+
+// App components
+import { getLocation } from '../location';
+
+// Styles
+import './Map.css';
+
+export default class Map extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 50.94544,
+      lng: -1.42896,
+      zoom: 13,
+    };
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    getLocation();
+    return (
+      <div className="Map">
+        <div id="startLat" />
+        <div id="startLon" />
+        <LeafletMap center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          {/* <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker> */}
+        </LeafletMap>
+      </div>
+    );
+  }
+}
